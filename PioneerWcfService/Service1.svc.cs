c2f0202af553e7c80133d5f54c166f22b230e4ab
susheel <mmsusheelbhargav@gmail.com>
2017-08-23 18:51:46 -0400

    request service created commit

---
+++ PioneerWcfService/Service1.svc.cs
@@ -0,0 +1,25 @@
+﻿using Models;
+using PioneerDataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace PioneerWcfService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        public int InsertEmployee(EmployeeDetails employee)
+        {
+            EmployeeDataAccess emp = new EmployeeDataAccess();
+            int det = emp.SaveEmployee(employee);
+            return det;
+
+        }
+    }
+}
